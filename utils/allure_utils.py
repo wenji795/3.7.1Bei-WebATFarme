54163162f03b180b33049beb8ca38f7a45a779b9
Wenji Shen <swjoanna795@gmail.com>
2025-11-09 21:23:17 +1300

    解释器版本不同的小bug

--- utils/allure_utils.py
+++ utils/allure_utils.py
@@ -4,4 +4,4 @@ import allure
 def allure_init(case):#case是形参
     allure.dynamic.feature(case["feature"])
     allure.dynamic.story(case["story"])
-    allure.dynamic.title(f"ID:{case["id"]} -- {case["title"]}")
+    allure.dynamic.title(f'ID:{case["id"]} -- {case["title"]}')
