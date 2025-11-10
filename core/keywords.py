11e29fa6416be035501ade3acdd85f0e4761649e
Wenji Shen <swjoanna795@gmail.com>
2025-11-10 14:51:05 +1300

    add: 关键字函数内添加了allure报告步骤和log

--- core/keywords.py
+++ core/keywords.py
@@ -1,4 +1,7 @@
+import logging
 import time
+
+import allure
 
 
 class Keywords:
@@ -13,21 +16,31 @@ class Keywords:
     # 核心操作关键字  每个关键字实际上是对应一个步骤
     def open(self, step):
         #打开网址
-        self.driver.get(step["data"])
+        with allure.step(f'第{step["step_num"]}步: {step["step_name"]}'):
+            logging.info(f'第{step["step_num"]}步: {step["step_name"]}')
+            self.driver.get(step["data"])
 
     def click(self, step):
         #点击
-        self.driver.find_element(step["by"], step["value"]).click()
+        with allure.step(f'第{step["step_num"]}步: {step["step_name"]}'):
+            logging.info(f'第{step["step_num"]}步: {step["step_name"]}')
+            self.driver.find_element(step["by"], step["value"]).click()
 
     def input(self, step):
         #输入文本
-        self.driver.find_element(step["by"], step["value"]).send_keys(step["data"])
+        with allure.step(f'第{step["step_num"]}步: {step["step_name"]}'):
+            logging.info(f'第{step["step_num"]}步: {step["step_name"]}')
+            self.driver.find_element(step["by"], step["value"]).send_keys(step["data"])
 
     def clear(self, step):
         #清空
-        self.driver.find_element(step["by"], step["value"]).clear()
+        with allure.step(f'第{step["step_num"]}步: {step["step_name"]}'):
+            logging.info(f'第{step["step_num"]}步: {step["step_name"]}')
+            self.driver.find_element(step["by"], step["value"]).clear()
 
 
     def wait(self, step):
         #等待
-        time.sleep(step["data"])
+        with allure.step(f'第{step["step_num"]}步: {step["step_name"]}'):
+            logging.info(f'第{step["step_num"]}步: {step["step_name"]}')
+            time.sleep(step["data"])
